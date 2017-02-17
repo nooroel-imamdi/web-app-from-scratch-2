1ae5a1c07c72ec5fe410de7c8a313fef438f170c
Oliver <hamburger602@gmail.com>
2017-02-17 10:54:30 +0100

    added proper route hash name

--- Week 2/Opdracht 1/static/app.js
+++ Week 2/Opdracht 1/static/app.js
@@ -2,17 +2,18 @@
     "use strict";
 
     var app = {
-        init: function () {
+        init: function() {
             routes.init();
             myData.init();
             sections.toggle("#app__list");
             onSearch.init();
+            createHTML.moreInfo();
         }
     };
 
     // Eventlistener for when the user clicks on a link and the hash changes
     var routes = {
-        init: function () {
+        init: function() {
             routie({
                 'app__home': function() {
                     sections.toggle(window.location.hash);
@@ -20,23 +21,19 @@
                 'app__list': function() {
                     sections.toggle(window.location.hash);
                 },
-                'more-info': function() {
-                    console.log("DEATAILLLsss");
-                    // toggle morre details page
-
+                'app__list/:id': function(id) {
+                    createHTML.hash(id);
+                    // Wat ik wil: #app_list/nl-SK-A-2860
                 }
             });
         }
     };
 
     var sections = {
-        toggle: function (route) {
-            var mySection = document.querySelectorAll('section');
-
-            mySection.forEach(function (el) {
+        toggle: function(route) {
+            document.querySelectorAll('section').forEach(function (el) {
                 if ('#' + el.id === route) {
                     el.classList.remove('section__hide');
-
                 } else {
                     el.classList.add('section__hide');
                 }
@@ -50,25 +47,25 @@
             document.getElementById("searchBar__btn").addEventListener('click', this.getQuery);
         },
         getQuery: function() {
-            var query = document.getElementById('searchBar').value;
-            myData.init(query);
+            myData.init(document.getElementById('searchBar').value);
         }
     };
 
-    // Gets dat from Rijksmuseum API
+    // Gets data from Rijksmuseum API
     var myData = {
         init: function (query) {
             var request = new window.XMLHttpRequest();
-            var searchQuery = query;
-            var api = "https://www.rijksmuseum.nl/api/nl/collection?q=" + searchQuery + "&key=NG2q9L0R&format=json";
+            var api = "https://www.rijksmuseum.nl/api/nl/collection?q=" + query + "&key=NG2q9L0R&format=json";
 
             request.open("GET", api, true);
             request.onload = function () {
                 if (request.status >= 200 && request.status < 400) {
                     // Success!
                     var data = JSON.parse(request.responseText);
-                    createHTML.search(data , searchQuery);
+                    createHTML.search(data);
+                    var artObj = data.artObjects;
                 } else {
+                    // No succes
                     console.log('We reached our target server, but it returned an error');
                 }
             };
@@ -81,11 +78,31 @@
         }
     };
 
-    // Get the data and render on the page what i want
+    // Get the data and render on the page a couple of art pieces
     // When user search -> Show information
     var createHTML = {
         search: function (data) {
             document.getElementById('artContainer').innerHTML = Handlebars.compile(document.getElementById("artObjects").innerHTML)(data);
+
+            [].forEach.call(document.getElementsByClassName("art__moreinfo"), this.moreInfo);
+        },
+        hash: function(hashname) {
+            console.log(hashname);
+            var hashId = hashname;
+        },
+        // More info function for when user selects more info
+        moreInfo: function(el) {
+            createHTML.hash();
+
+            console.log(sasas);
+
+            if("#" + el === window.location.hash) {
+                routie({
+                    '*': function() {
+                        console.log(window.location.hash);
+                    }
+                })
+            }
         }
     };
 
